bd52a1cb2da51b82233fad262a979e1646673a0f
mhd-sulu-786 <muhammedsulaimant162@gmail.com>
2023-10-22 14:08:54 +0530

    my first blog app nav bar responsive section

---
+++ script.js
@@ -0,0 +1,12 @@
+//styles
+const nav_btn = document.getElementById('nav-icon-btn');
+const nav_off_btn =document.getElementById('nav-off');
+nav_btn.addEventListener("click",()=>{
+    document.querySelector("#nav").style.display="block";
+    nav_btn.style.display="none";
+    nav_off_btn.style.display="block";
+})
+nav_off_btn.addEventListener("click",()=>{
+    document.querySelector("#nav").style.display="none";
+    nav_btn.style.display="block";
+})

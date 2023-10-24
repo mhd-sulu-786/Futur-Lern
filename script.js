449eb8cb2f94c50bf023320343b23092e1be262a
mhd-sulu-786 <muhammedsulaimant162@gmail.com>
2023-10-24 19:30:34 +0530

    it finished vidition only blnc

--- script.js
+++ script.js
@@ -17,3 +17,51 @@ nav_off_btn.addEventListener("click",()=>{
     nav_btn.style.display="block";
 
  }
+ //validetion
+ const name_err=document.getElementById('name-err');
+ const err_email=document.getElementById('name-email');
+ const err_phone=document.getElementById('name-phone');
+ const err_messege=document.getElementById('name-messege');
+ const err_submit=document.getElementById('name-submit');
+ function validition_name(){
+    const name =document.getElementById('name').value;
+    if(name.length == 0){
+        name_err.style.color="red";
+        name_err.innerHTML= "Name is required";
+       return false;
+    }
+    else if(!name.match(/^[A-Za-z]*\s[A-Za-z]*&/)){
+        name_err.style.color="red";
+       name_err.innerHTML= "Name is required";
+       return false;
+    } 
+     name_err.style.color="green";
+    name_err.innerHTML= "Name is Valid";
+        
+    return true
+    
+
+ }
+ function validition_phone(){
+    const phone =document.getElementById('phone').value;
+    if(phone.length == 0){
+        err_phone.style.color="red";
+        err_phone.innerHTML= "Phone number is required";
+       return false;
+    }
+    else if(!phone.match(/^[0-9]$(10)&/)){
+        err_phone.style.color="red";
+       err_phone.innerHTML= "Phone number is required";
+       return false;
+    }
+    else if(phone.length !==10){
+         err_phone.style.color="red";
+        err_phone.innerHTML= "Phone number is required";
+        
+        return false;
+    }
+    
+
+
+ }
+
